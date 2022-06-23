595abed6a210d3686fe2b07f10744090cf4525cb
Zachary Kerr <zacharykerr@ufl.edu>
2022-06-23 01:08:59 -0400

    Add files via upload

--- volts/errors/handlers.py
+++ volts/errors/handlers.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_wtf.csrf import CSRFError
 
 errors = Blueprint('errors', __name__)
 
@@ -10,9 +11,14 @@ def error_404(error):
 
 @errors.app_errorhandler(403)
 def error_403(error):
-    return render_template('errors/404.html'), 403
+    return render_template('errors/403.html'), 403
 
 
 @errors.app_errorhandler(500)
 def error_500(error):
-    return render_template('errors/404.html'), 500
+    return render_template('errors/500.html'), 500
+
+
+@errors.errorhandler(CSRFError)
+def error_csrf(error):
+    return render_template('errors/403.html', reason=error.description), 400
